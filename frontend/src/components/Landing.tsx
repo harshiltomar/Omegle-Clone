e029c55d1c2719f8f4459b58ddb03ccadaa06a3c
Harshil Tomar <harshiltomar20@gmail.com>
2024-07-01 16:08:23 +0530

    feat: landing, room basic

---
+++ frontend/src/components/Landing.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+const Landing = () => {
+    const[name, setName] = useState("");
+
+    return <div>
+        <input type="text" onChange={(e) => {
+            setName(e.target.value);
+        }}>
+        </input>
+        <Link to={`/room/?name=${name}`}>Join</Link>
+    </div>
+}
+
+export default Landing
